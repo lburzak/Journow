051af142a72afdefd6cb5530f2f176ca99ed2df0
polydome <polydome@outlook.com>
2020-11-04 13:04:16 +0100

    Create TimeParser class

---
+++ time-date-parser/src/main/java/com/github/polydome/journow/timedateparser/TimeParser.java
@@ -0,0 +1,13 @@
+package com.github.polydome.journow.timedateparser;
+
+import java.util.Optional;
+
+public class TimeParser {
+    public Optional<TimeRange> parseRange() {
+        return Optional.empty();
+    }
+
+    public Optional<Long> parseDuration() {
+        return Optional.empty();
+    }
+}
