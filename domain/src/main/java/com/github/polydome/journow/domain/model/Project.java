cd02278d9ed313ec87eb828bb32e9ddaae159ce9
polydome <polydome@outlook.com>
2021-01-16 11:25:27 +0100

    Create project model

---
+++ domain/src/main/java/com/github/polydome/journow/domain/model/Project.java
@@ -0,0 +1,11 @@
+package com.github.polydome.journow.domain.model;
+
+public class Project {
+    private final long id;
+    private final String name;
+
+    public Project(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
