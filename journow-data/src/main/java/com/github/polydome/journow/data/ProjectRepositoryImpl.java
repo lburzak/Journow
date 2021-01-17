6b43a975c3f56d22f35190750b21ea76666234c1
polydome <polydome@outlook.com>
2021-01-17 10:48:34 +0100

    Add return value to ProjectRepository insert method

--- journow-data/src/main/java/com/github/polydome/journow/data/ProjectRepositoryImpl.java
+++ journow-data/src/main/java/com/github/polydome/journow/data/ProjectRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.github.polydome.journow.data;
 
 import com.github.polydome.journow.domain.model.Project;
-import com.github.polydome.journow.domain.model.Session;
 import com.github.polydome.journow.domain.repository.ProjectRepository;
 
 import java.sql.Connection;
@@ -10,9 +9,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.github.polydome.journow.data.ResultSetUtil.parseProject;
-import static com.github.polydome.journow.data.ResultSetUtil.parseSession;
 
 public class ProjectRepositoryImpl implements ProjectRepository {
     private final Database database;
@@ -20,6 +19,7 @@ public class ProjectRepositoryImpl implements ProjectRepository {
     private PreparedStatement selectAll;
     private PreparedStatement insertWithId;
     private PreparedStatement insertNew;
+    private PreparedStatement findById;
 
     public ProjectRepositoryImpl(Database database) {
         this.database = database;
@@ -49,7 +49,7 @@ public class ProjectRepositoryImpl implements ProjectRepository {
     }
 
     @Override
-    public void insert(Project project) {
+    public Project insert(Project project) {
         if (!database.isReady())
             throw new IllegalStateException("Database is not ready");
 
@@ -60,19 +60,57 @@ public class ProjectRepositoryImpl implements ProjectRepository {
 
                 insertWithId.setLong(1, project.getId());
                 insertWithId.setString(2, project.getName());
-
                 insertWithId.execute();
+
+                Optional<Project> insertedProject = findById(project.getId());
+                if (insertedProject.isPresent())
+                    return insertedProject.get();
             } else {
                 if (insertNew == null)
                     insertNew = getConnection().prepareStatement("insert into project (project_name) values (?)");
 
                 insertNew.setString(1, project.getName());
                 insertNew.execute();
+
+                try (ResultSet generatedKeys = insertNew.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        long id = generatedKeys.getLong(1);
+                        Optional<Project> insertedProject = findById(id);
+                        if (insertedProject.isPresent())
+                            return insertedProject.get();
+                    }
+                }
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return null;
+    }
+
+    private Optional<Project> findById(long id) {
+        if (!database.isReady())
+            throw new IllegalStateException("Database is not ready");
+
+        try {
+            if (findById == null)
+                findById = getConnection().prepareStatement("select * from project where project_id = ?");
+
+            findById.setLong(1, id);
+
+            try (var rs = findById.executeQuery()) {
+                if (rs.next()) {
+                    Project project = parseProject(rs);
+                    if (project != null)
+                        return Optional.of(project);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
     }
 
     private Connection getConnection() throws SQLException {
