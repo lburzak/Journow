a1fe9b8934c510c1ec920f05a651a9e986b4bdf4
polydome <polydome@outlook.com>
2021-01-18 09:43:29 +0100

    Make session list a table

---
+++ ui/src/main/java/com/github/polydome/journow/ui/listmodel/SessionTableModel.java
@@ -0,0 +1,79 @@
+package com.github.polydome.journow.ui.listmodel;
+
+import com.github.polydome.journow.data.event.DataEvent;
+import com.github.polydome.journow.domain.model.Session;
+import com.github.polydome.journow.domain.repository.SessionRepository;
+import com.github.polydome.journow.ui.tracker.common.FormatUtils;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.swing.table.AbstractTableModel;
+import java.time.Duration;
+import java.util.List;
+
+public class SessionTableModel extends AbstractTableModel {
+    private final List<Session> sessions;
+
+    @Inject
+    public SessionTableModel(SessionRepository sessionRepository, @Named("SessionDataEvents") Observable<DataEvent> events) {
+        sessions = sessionRepository.findAll();
+        events.subscribe(ev -> {
+            if (ev.getType() == DataEvent.Type.INSERT) {
+                List<Session> freshTasks = sessionRepository.findAll();
+                sessions.clear();
+                sessions.addAll(freshTasks);
+                var insertedTask = sessions.stream().filter(task -> task.getId() == ev.getIdStart()).findFirst();
+                if (insertedTask.isPresent()) {
+                    var index = sessions.indexOf(insertedTask.get());
+                    fireTableRowsInserted(index, index);
+                } else {
+                    System.err.println("Inserted session not found");
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getRowCount() {
+        return sessions.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Session session = sessions.get(row);
+        switch (col) {
+            case 0:
+                return session.getStartedAt();
+            case 1:
+                return session.getEndedAt();
+            case 2:
+                return FormatUtils.formatDuration(Duration.between(session.getStartedAt(), session.getEndedAt()));
+            case 3:
+                return session.getTask().getTitle();
+        }
+
+        return "";
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return "Date started";
+            case 1:
+                return "Date ended";
+            case 2:
+                return "Duration";
+            case 3:
+                return "Task";
+        }
+
+        return "";
+    }
+}
--- ui/src/main/java/com/github/polydome/journow/ui/tracker/SessionListView.java
+++ ui/src/main/java/com/github/polydome/journow/ui/tracker/SessionListView.java
@@ -1,51 +1,23 @@
 package com.github.polydome.journow.ui.tracker;
 
-import com.github.polydome.journow.domain.model.Session;
-import com.github.polydome.journow.ui.listmodel.SessionListModel;
-import com.github.polydome.journow.ui.tracker.common.FormatUtils;
+import com.github.polydome.journow.ui.listmodel.SessionTableModel;
 
 import javax.inject.Inject;
 import javax.swing.*;
 import java.awt.*;
-import java.time.Duration;
 
 public class SessionListView extends JPanel {
     @Inject
-    public SessionListView(SessionListModel model) {
-        JList<Session> list = new JList<>(model);
-        list.setFixedCellHeight(40);
-        list.setFixedCellWidth(280);
-        list.setCellRenderer(new SessionCellRenderer());
-        add(list);
-        add(new JScrollPane(list));
-    }
+    public SessionListView(SessionTableModel model) {
+        setLayout(new GridBagLayout());
+        var constraints = new GridBagConstraints();
 
-    private static class SessionCellRenderer extends DefaultListCellRenderer {
-        @Override
-        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            Session session = (Session) value;
-            JPanel cell = new JPanel();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 1;
+        constraints.weighty = 1;
+        JTable table = new JTable(model);
 
-            cell.setLayout(new GridBagLayout());
-            GridBagConstraints constraints = new GridBagConstraints();
-            constraints.fill = GridBagConstraints.VERTICAL;
-            constraints.insets = new Insets(5, 5, 5, 5);
-
-            Duration duration = Duration.between(session.getStartedAt(), session.getEndedAt());
-            JLabel durationText = new JLabel(FormatUtils.formatDuration(duration));
-            constraints.gridx = 0;
-            constraints.anchor = GridBagConstraints.PAGE_START;
-            cell.add(durationText, constraints);
-
-            JLabel titleText = new JLabel(session.getTask().getTitle());
-            titleText.setHorizontalAlignment(LEFT);
-            constraints.gridx = 1;
-            constraints.weightx = 1;
-            constraints.anchor = GridBagConstraints.PAGE_START;
-            constraints.fill = GridBagConstraints.BOTH;
-            cell.add(titleText, constraints);
-
-            return cell;
-        }
+        add(table, constraints);
+        add(new JScrollPane(table), constraints);
     }
 }
