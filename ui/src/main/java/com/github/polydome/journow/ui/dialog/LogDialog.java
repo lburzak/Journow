f4e8722ec2a66a37080a8682f5f8f1ca17b7bf5d
polydome <polydome@outlook.com>
2021-01-19 11:49:20 +0100

    Make log dialog always on top

--- ui/src/main/java/com/github/polydome/journow/ui/dialog/LogDialog.java
+++ ui/src/main/java/com/github/polydome/journow/ui/dialog/LogDialog.java
@@ -2,12 +2,14 @@ package com.github.polydome.journow.ui.dialog;
 
 import javax.swing.*;
 
-public class LogDialog extends JFrame {
+public class LogDialog extends JDialog {
     public LogDialog() {
         setTitle("Journow - Log session");
 
         setLocationRelativeTo(null);
 
+        setAlwaysOnTop(true);
+        setModal(true);
         setSize(100, 100);
         setVisible(true);
     }
