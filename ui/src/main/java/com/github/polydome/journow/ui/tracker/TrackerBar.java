119a787c0dc70e193b063c0a79ee6684329c2358
polydome <polydome@outlook.com>
2021-01-19 09:55:54 +0100

    Add track/log buttons to layout

--- ui/src/main/java/com/github/polydome/journow/ui/tracker/TrackerBar.java
+++ ui/src/main/java/com/github/polydome/journow/ui/tracker/TrackerBar.java
@@ -1,16 +1,12 @@
 package com.github.polydome.journow.ui.tracker;
 
-import com.github.polydome.journow.domain.exception.TrackerNotRunningException;
 import com.github.polydome.journow.viewmodel.TrackerViewModel;
 
 import javax.inject.Inject;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-
-import static javax.swing.SpringLayout.*;
 
 public class TrackerBar extends JPanel {
     private final SpringLayout layout = new SpringLayout();
@@ -22,21 +18,28 @@ public class TrackerBar extends JPanel {
     JButton stopTrackerButton = new JButton("Stop");
     JButton startTrackerButton = new JButton("Start");
     JButton logButton = new JButton("Log");
-    JComboBox<String> projectSelection = new JComboBox<>();
+    JComboBox<String> projectSelector = new JComboBox<>();
 
     private void inflateLayout() {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.BOTH;
-        constraints.insets = new Insets(0, 10, 0, 10);
+        constraints.insets = new Insets(0, 10, 0, 0);
         constraints.weightx = 0.1;
         add(elapsedTimeCounter, constraints);
 
         constraints.weightx = 0.8;
         add(taskTitleLabel, constraints);
         add(taskTitleInput, constraints);
+        constraints.gridy = 1;
+        add(projectSelector, constraints);
 
+        constraints.insets = new Insets(0, 10, 0, 10);
+        constraints.gridy = 0;
         constraints.weightx = 0.1;
         add(stopTrackerButton, constraints);
+        add(startTrackerButton, constraints);
+        constraints.gridy = 1;
+        add(logButton, constraints);
     }
 
     public void onCreate() {
@@ -73,6 +76,9 @@ public class TrackerBar extends JPanel {
                     elapsedTimeCounter.setVisible(it);
                     stopTrackerButton.setVisible(it);
                     taskTitleInput.setVisible(!it);
+                    projectSelector.setVisible(!it);
+                    startTrackerButton.setVisible(!it);
+                    logButton.setVisible(!it);
                 });
 
         setVisible(true);
