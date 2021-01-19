1cf766073cc7f81b7222a79a214016d6853a071c
polydome <polydome@outlook.com>
2021-01-19 11:18:30 +0100

    Add tracker bar buttons handlers

---
+++ ui/src/main/java/com/github/polydome/journow/ui/dialog/LogDialog.java
@@ -0,0 +1,14 @@
+package com.github.polydome.journow.ui.dialog;
+
+import javax.swing.*;
+
+public class LogDialog extends JFrame {
+    public LogDialog() {
+        setTitle("Journow - Log session");
+
+        setLocationRelativeTo(null);
+
+        setSize(100, 100);
+        setVisible(true);
+    }
+}
