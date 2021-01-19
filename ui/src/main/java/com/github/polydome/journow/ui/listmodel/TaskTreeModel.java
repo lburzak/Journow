edc1388e1571d414eb9fa077833ec1a4e187f7d8
polydome <polydome@outlook.com>
2021-01-19 19:28:48 +0100

    Redesign tasks list as tree

---
+++ ui/src/main/java/com/github/polydome/journow/ui/listmodel/TaskTreeModel.java
@@ -0,0 +1,68 @@
+package com.github.polydome.journow.ui.listmodel;
+
+import com.github.polydome.journow.domain.model.Project;
+import com.github.polydome.journow.domain.model.Task;
+import com.github.polydome.journow.domain.repository.TaskRepository;
+
+import javax.inject.Inject;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class TaskTreeModel extends DefaultTreeModel {
+
+    // A bit hacky way to retrieve selected item
+    private final Map<String, Object> labelItemMap = new HashMap<>();
+
+    @Inject
+    public TaskTreeModel(TaskRepository taskRepository) {
+        super(new DefaultMutableTreeNode());
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode) getRoot();
+
+        Map<Project, List<Task>> projectTaskMap = new HashMap<>();
+
+        List<Task> tasks = taskRepository.findAll();
+
+        for (var task : tasks) {
+            if (!projectTaskMap.containsKey(task.getProject()))
+                projectTaskMap.put(task.getProject(), new LinkedList<>());
+
+            projectTaskMap.get(task.getProject()).add(task);
+        }
+
+        var orphanTasks = projectTaskMap.remove(null);
+
+        for (var project : projectTaskMap.keySet()) {
+            DefaultMutableTreeNode node = new DefaultMutableTreeNode(formatProject(project));
+
+            for (var task : projectTaskMap.get(project)) {
+                node.add(new DefaultMutableTreeNode(formatTask(task)));
+            }
+
+            root.add(node);
+        }
+
+        for (var task : orphanTasks) {
+            root.add(new DefaultMutableTreeNode(formatTask(task)));
+        }
+    }
+
+    private String formatProject(Project project) {
+        String label = "<html><font color=#AAAAAA>#" + project.getId() +" <font color=#000000>" + project.getName() + "</html>";
+        labelItemMap.put(label, project);
+        return label;
+    }
+
+    private String formatTask(Task task) {
+        String label = "<html><font color=#AAAAAA>#" + task.getId() +" <font color=#000000>" + task.getTitle() + "</html>";
+        labelItemMap.put(label, task);
+        return label;
+    }
+
+    public Object getObjectByLabel(String label) {
+        return labelItemMap.get(label);
+    }
+}
--- ui/src/main/java/com/github/polydome/journow/ui/tracker/TaskListView.java
+++ ui/src/main/java/com/github/polydome/journow/ui/tracker/TaskListView.java
@@ -2,42 +2,45 @@ package com.github.polydome.journow.ui.tracker;
 
 import com.github.polydome.journow.domain.model.Task;
 import com.github.polydome.journow.ui.listmodel.TaskListModel;
+import com.github.polydome.journow.ui.listmodel.TaskTreeModel;
 import com.github.polydome.journow.ui.popup.TaskPopupMenu;
 import com.github.polydome.journow.ui.preview.PreviewModel;
 
 import javax.inject.Inject;
 import javax.swing.*;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class TaskListView extends JPanel {
     @Inject
-    public TaskListView(TaskListModel model, TaskPopupMenu taskPopupMenu, PreviewModel previewModel) {
-        JList<Task> list = new JList<>(model);
-        list.setFixedCellHeight(40);
-        list.setFixedCellWidth(280);
-        list.addMouseListener(new TaskCellPopupListener(taskPopupMenu, model));
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.addListSelectionListener(listSelectionEvent -> {
-            if (!listSelectionEvent.getValueIsAdjusting()) {
-                Task selectedTask = list.getModel()
-                        .getElementAt(list.getSelectedIndex());
-
-                previewModel.previewTask(selectedTask);
+    public TaskListView(TaskTreeModel model, TaskPopupMenu taskPopupMenu, PreviewModel previewModel) {
+        JTree tree = new JTree(model);
+        tree.setRootVisible(false);
+        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        tree.addMouseListener(new TaskCellPopupListener(taskPopupMenu, model));
+        tree.addTreeSelectionListener(ev -> {
+            TreePath path = ev.getNewLeadSelectionPath();
+            if (path != null) {
+                Object item = model.getObjectByLabel(path.getLastPathComponent().toString());
+                if (item instanceof Task) {
+                    previewModel.previewTask((Task) item);
+                }
             }
         });
 
-        JScrollPane scrollPane = new JScrollPane(list);
+        JScrollPane scrollPane = new JScrollPane(tree);
         setLayout(new BorderLayout());
         add(scrollPane, BorderLayout.CENTER);
     }
 
     private static class TaskCellPopupListener extends MouseAdapter {
         private final TaskPopupMenu popupMenu;
-        private final TaskListModel model;
+        private final TaskTreeModel model;
 
-        private TaskCellPopupListener(TaskPopupMenu popupMenu, TaskListModel model) {
+        private TaskCellPopupListener(TaskPopupMenu popupMenu, TaskTreeModel model) {
             this.popupMenu = popupMenu;
             this.model = model;
         }
@@ -45,10 +48,14 @@ public class TaskListView extends JPanel {
         @Override
         public void mousePressed(MouseEvent e) {
             if (SwingUtilities.isRightMouseButton(e)) {
-                JList<Task> list = (JList<Task>) e.getSource();
-                int row = list.locationToIndex(e.getPoint());
-                list.setSelectedIndex(row);
-                popupMenu.show(e.getComponent(), e.getX(), e.getY(), model.getElementAt(row));
+                JTree tree = (JTree) e.getSource();
+                TreePath tp = tree.getPathForLocation(e.getX(), e.getY());
+                if (tp != null) {
+                    Object selectedObj = model.getObjectByLabel(tp.getLastPathComponent().toString());
+                    if (selectedObj instanceof Task)
+                        popupMenu.show(e.getComponent(), e.getX(), e.getY(), (Task) selectedObj);
+                    tree.setSelectionPath(tp);
+                }
             }
         }
     }
