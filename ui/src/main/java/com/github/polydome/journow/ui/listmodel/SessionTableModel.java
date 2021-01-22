db49146afeccaa3a017a84bb634f52d1466da3f2
polydome <polydome@outlook.com>
2021-01-22 15:14:23 +0100

    Improve datetime formatting in sessions table

--- ui/src/main/java/com/github/polydome/journow/ui/listmodel/SessionTableModel.java
+++ ui/src/main/java/com/github/polydome/journow/ui/listmodel/SessionTableModel.java
@@ -10,10 +10,17 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.swing.table.AbstractTableModel;
 import java.time.Duration;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 public class SessionTableModel extends AbstractTableModel {
     private final List<Session> sessions;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+            .ofPattern("dd-MM-yyyy hh:mm")
+            .withLocale( Locale.getDefault() )
+            .withZone( ZoneId.systemDefault() );
 
     @Inject
     public SessionTableModel(SessionRepository sessionRepository, @Named("SessionDataEvents") Observable<DataEvent> events) {
@@ -49,9 +56,9 @@ public class SessionTableModel extends AbstractTableModel {
         Session session = sessions.get(row);
         switch (col) {
             case 0:
-                return session.getStartedAt();
+                return dateTimeFormatter.format(session.getStartedAt());
             case 1:
-                return session.getEndedAt();
+                return dateTimeFormatter.format(session.getEndedAt());
             case 2:
                 return FormatUtils.formatDuration(Duration.between(session.getStartedAt(), session.getEndedAt()));
             case 3:
