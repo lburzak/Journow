788709dcd06ba62bf049a2328d82a081144f7f00
polydome <polydome@outlook.com>
2021-01-22 15:14:45 +0100

    Resize and align columns in session table

--- ui/src/main/java/com/github/polydome/journow/ui/tracker/SessionListView.java
+++ ui/src/main/java/com/github/polydome/journow/ui/tracker/SessionListView.java
@@ -4,9 +4,14 @@ import com.github.polydome.journow.ui.listmodel.SessionTableModel;
 
 import javax.inject.Inject;
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 
 public class SessionListView extends JPanel {
+    private static final int[] COLUMN_WIDTHS = {100, 100, 20, 500};
+
     @Inject
     public SessionListView(SessionTableModel model) {
         setLayout(new GridBagLayout());
@@ -17,7 +22,24 @@ public class SessionListView extends JPanel {
         constraints.weighty = 1;
         JTable table = new JTable(model);
 
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+        TableCellRenderer[] cellRenderers =
+                new TableCellRenderer[]{centerRenderer, centerRenderer, centerRenderer, null};
+
+        TableColumn column;
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            column = table.getColumnModel().getColumn(i);
+
+            if (cellRenderers[i] != null)
+                column.setCellRenderer(cellRenderers[i]);
+            column.setPreferredWidth(COLUMN_WIDTHS[i]);
+
+        }
+
         add(table, constraints);
         add(new JScrollPane(table), constraints);
+
     }
 }
