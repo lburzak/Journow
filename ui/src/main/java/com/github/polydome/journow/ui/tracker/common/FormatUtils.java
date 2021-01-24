9f1eb8ff758c667393e788912fb98727e5cae023
polydome <polydome@outlook.com>
2021-01-24 08:23:53 +0100

    Show hours in sessions table

--- ui/src/main/java/com/github/polydome/journow/ui/tracker/common/FormatUtils.java
+++ ui/src/main/java/com/github/polydome/journow/ui/tracker/common/FormatUtils.java
@@ -4,6 +4,6 @@ import java.time.Duration;
 
 public class FormatUtils {
     public static String formatDuration(Duration duration) {
-        return String.format("%02d:%02d", duration.toMinutesPart(), duration.toSecondsPart());
+        return String.format("%02d:%02d:%02d", duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart());
     }
 }
